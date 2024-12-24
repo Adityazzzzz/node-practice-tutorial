abb740144844758c7b45e0c9cda9a7bcbbe59496
Aditya Singh <160380902+Adityazzzzz@users.noreply.github.com>
2024-12-24 16:45:35 +0530

    8th

--- 2-Express tutorial/app.js
+++ 2-Express tutorial/app.js
@@ -1,20 +1,11 @@
 const express = require('express');
 const app =express();
-const path =require('path');
 
-app.use(express.static('./public'));// *************imp
+const data = require('./data.js');
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./navbar/index.html'));
+    res.json(data.products);
 })
-app.get('*',(req,res)=>{
-    res.status(404).send('Res not found');
-})
-
-
-
-
-
 
 app.listen(5000,()=>{
     console.log('go to localhost:5000');
