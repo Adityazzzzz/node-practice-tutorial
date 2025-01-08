b3cb44c462a0bf53d73317ede99f4a929a985d2b
Aditya Singh <160380902+Adityazzzzz@users.noreply.github.com>
2025-01-08 07:29:11 +0530

    30th

---
+++ 5-jsonwebtoken/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+require('express-async-errors');
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+// middleware
+app.use(express.static('./public'));
+app.use(express.json());
+
+
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+
+
+
+const port = process.env.PORT || 3000;
+const start = async () => {
+  try {
+    app.listen(port);
+  } 
+  catch (error) {
+    console.log(error);
+  }
+};
+
+start();
