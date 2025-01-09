5febe083e08df5805e82f033a4064c6819944f31
Aditya Singh <160380902+Adityazzzzz@users.noreply.github.com>
2025-01-09 13:48:09 +0530

    32nd

--- 5-jsonwebtoken/controllers/main.js
+++ 5-jsonwebtoken/controllers/main.js
@@ -1,40 +1,43 @@
-const CustomAPIError=require('../errors/custom-error')
 const jwt = require('jsonwebtoken')
+const {BadRequesterror} =require('../errors')
+const {Unauthenticatederror} = require('../errors')
+
+
+
 
 
 const login=async (req,res)=>{
   const {username, password}=req.body;
   if(!username || !password){
-    throw new CustomAPIError('Please provide email and password',400)
+    throw new BadRequesterror('Please provide email and password')
   }
-  console.log(username, password);
-
   const id= new Date().getDate()
-  const token = jwt.sign({id,username},process.env.JWT_SECRET,{expiresIn:'30d'}) // we can pass infinite value in jwt.sign  
-
-  res.status(200).json({msg:'fake-login/register/signup-route',token})
+  const token = jwt.sign({id,username},process.env.JWT_SECRET,{expiresIn:'30d'}) 
+  // we can pass infinite value in jwt.sign  
+  res.status(200).json({msg:'Access Granted',token})
 }
 
 
 
+
+
+
 const dashboard = async(req,res)=>{
-  
+
   const authHeader = req.headers.authorization
   if(!authHeader|| !authHeader.startsWith('Bearer ')){
-    throw new CustomAPIError('No token provider',401)
+    throw new Unauthenticatederror('No token provider')
   }
-  // console.log(req.headers);
-  const token = authHeader.split(' ')[1]
 
+  const token = authHeader.split(' ')[1]
   try {
     const decoded =jwt.verify(token,process.env.JWT_SECRET)
     const luckynumber = Math.floor(Math.random()*100)
     res.status(200).json({msg:`Hello ${decoded.username}`, secret:`Here is your authorize data, your lucky number is ${luckynumber}`} )   
   } 
   catch (error) {
-    throw new CustomAPIError('Not authorized',401)
+    throw new Unauthenticatederror('Not authorized')
   }
-
 }
 
 
---
+++ 5-jsonwebtoken/errors/index.js
@@ -0,0 +1,6 @@
+const CustomAPIError =require('./custom-error')
+const BadRequesterror =require('./bad-request')
+const Unauthenticatederror = require('./unauthenticated')
+
+
+module.exports ={CustomAPIError, BadRequesterror, Unauthenticatederror}
