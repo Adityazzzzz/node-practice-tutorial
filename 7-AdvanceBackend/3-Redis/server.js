c1ece2a2d423117fa80b332f963c0c316b4a9311
Aditya Singh <adityasinghrajawat2004@gmail.com>
2025-09-13 17:29:48 +0530

    Redis-3

--- 7-AdvanceBackend/3-Redis/server.js
+++ 7-AdvanceBackend/3-Redis/server.js
@@ -13,12 +13,28 @@ async function testRedisConnection(){
     try{
         await client.connect()
         console.log('Connected to redis')
+
+        // 1 - key,value pairs
+        await client.set("key","value");
+        const extractvalue = await client.get("key")
+
+        console.log(extractvalue)
+        
+        // 2- for deleting 
+        await client.del("key")
+        
+        // 3
+        const count = await client.set('num',"100")
+        console.log(await client.incr('num')) // 101
+        console.log(await client.decr('num')) // 100
+
+
     } 
     catch(error){
         console.error(error);
     }
     finally{
-        await client.quit();
+        await client.quit(); // sometimes we leave open connections, it will make them stop
     }
 }
 
