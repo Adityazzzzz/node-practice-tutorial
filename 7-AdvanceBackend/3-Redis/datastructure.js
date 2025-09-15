2946dce563a4366e5672aeec7766fdebe1c91282
Aditya Singh <adityasinghrajawat2004@gmail.com>
2025-09-15 23:10:42 +0530

    Redis-5

--- 7-AdvanceBackend/3-Redis/datastructure.js
+++ 7-AdvanceBackend/3-Redis/datastructure.js
@@ -26,19 +26,51 @@ async function RedisDS() {
         console.log(email, age, country);
 
 
+
+        
         // 2. vector -> lpush, rpush, lpop, rpop, lrange
 
         await client.lPush('data', ['note1', 'note2', 'note3']);
-        let arr = await client.lRange('data', 0, -1);
+
+        let listarr = await client.lRange('data', 0, -1);
+
         await client.lPush('data', 'note4');
 
-        const arr2 = await client.lPop("notes");
-        console.log(arr2)
+        const listarr2 = await client.lPop("notes");
 
 
 
 
+        // 3.sets -> sADD, sMEMBER, sISMEMBER, sREM
+        await client.sAdd('user:nickName',['john','varun','xyz'])
+        const extractnicknames = await client.sMembers('user:nickName') 
+        
+        // to search
+        const isVarunPresent = await client.sIsMember('user:nickName','varun')
 
+        // to remove
+        await client.sRem('user:nickName','xyz')
+
+
+
+
+        // 4.Sorted sets -> zAdd, zRange, zRank, zRem
+
+        // 5.Hash ->  hSET, hGET, hGETALL, hDEL
+        await client.hSet("product:1", {
+            name: "Product 1",
+            description: "product one description",
+            rating: "5",
+        });
+
+        const getProductRating = await client.hGet("product:1", "rating");
+
+        const getAllProductDetails = await client.hGetAll("product:1");
+       
+        await client.hDel("product:1", "rating");
+
+        const updatedProductDetails = await client.hGetAll("product:1");
+       
 
 
     }
