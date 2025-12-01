7d0473ffa23e75b16fb737f6577c0fe9de06fa10
Aditya Singh <adityasinghrajawat2004@gmail.com>
2025-12-01 14:01:16 +0530

    Complete 4 services

--- 7-AdvanceBackend/4-MicroServices/media-service/src/utils/cloudinary.js
+++ 7-AdvanceBackend/4-MicroServices/media-service/src/utils/cloudinary.js
@@ -24,14 +24,15 @@ const uploadMediatoCloud = (file)=>{
 };
 
 const deleteMediaFromCloudinary = async (publicId) => {
-  try {
-    const result = await cloudinary.uploader.destroy(publicId);
-    logger.info("Media deleted successfuly from cloud stroage", publicId);
-    return result;
-  } catch (error) {
-    logger.error("Error deleting media from cludinary", error);
-    throw error;
-  }
+    try {
+        const result = await cloudinary.uploader.destroy(publicId);
+        logger.info("Media deleted successfuly from cloud stroage", publicId);
+        return result;
+    } 
+    catch (error) {
+        logger.error("Error deleting media from cludinary", error);
+        throw error;
+    }
 };
 
 module.exports = { uploadMediaToCloudinary, deleteMediaFromCloudinary };
